b19e3d7118d41762895cfa7767385607660e2038
Tim <pub@lalatte.com>
2017-04-05 00:07:22 +0800

    Update

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var configIoT = {
     "clientId": config.iotClientId,
     "region": config.iotRegion,
     "host": config.iotEndpoint,
-    "reconnectPeriod": 300,
+    "reconnectPeriod": 5 //300,
 };
 
 var thingState = {
@@ -27,12 +27,17 @@ var thingState = {
         value: null
     },
     uv: {
-    	volts: null,
-    	intensity: null
+        volts: null,
+        intensity: null
     },
     text: {
         line1: null,
-        line2: null
+        line2: null,
+        color: {
+            r: null,
+            g: null,
+            b: null
+        }
     }
 };
 
@@ -41,63 +46,93 @@ console.log(thingState);
 console.log('[SETUP] Initializing IoT thingShadow with config:');
 console.log(configIoT);
 
+var clientTokens = [];
 
-
-function refreshShadow() {
-    var toUpdate = {
-        state: {
-            reported: thingState
-        }
-    };
+function refreshShadow(toUpdate) {
 
     console.log('[EVENT] refreshShadow(): Refhreshing the Shadow:');
     console.log(toUpdate);
 
 
-    thingShadow.update(config.iotThingName, toUpdate);
+    var clientTokenUpdate = thingShadow.update(config.iotThingName, {
+        state: {
+            reported: toUpdate
+        }
+    });
+    console.log('clientTokenUpdate:', clientTokenUpdate);
+    if (clientTokenUpdate === null) {
+        console.log('[EVENT] refreshShadow(): update shadow failed, operation still in progress');
+    }
+
 }
 
+function refreshLCD(text) {
 
+    if (text.line1 && text.line2 && text.color) {
+
+        env.lcd.setColor(text.color.r, text.color.g, text.color.b); // blue: 53, 39, 249
+
+        env.lcd.setCursor(0, 0);
+        env.lcd.write(text.line1);  
+        env.lcd.setCursor(1, 0);
+        env.lcd.write(text.line2);
+
+    }
+
+}
+
+const TEMPERATURE_FREQ = 60;
+const LIGHT_FREQ = 30;
 
 
 setInterval(function() {
-    var temperature = env.th02.getTemperature();
-    var humidity = env.th02.getHumidity();
-    if (temperature != thingState.temperature || humidity != thingState.humidity) {
-        console.log('[EVENT]: Temperature:', temperature, 'celcius');
-        console.log('[EVENT]: Humidity:', humidity, '%');
-        thingState.temperature = temperature;
-        thingState.humidity = humidity;
-        refreshShadow();
+    var temp = {
+        temperature: env.th02.getTemperature(),
+        humidity: env.th02.getHumidity()
+    };
+    console.log('[EVENT] Temperature:', temp.temperature, 'celcius');
+    console.log('[EVENT] Humidity:', temp.humidity, '%');
+    if (temp.temperature != thingState.temperature || temp.humidity != thingState.humidity) {
+        thingState.temperature = temp.temperature;
+        thingState.humidity = temp.humidity;
+        refreshShadow(temp);
     }
-}, 60000);
+}, TEMPERATURE_FREQ * 1000);
 
 var measureLight = false;
 setInterval(function() {
 
-	measureLight = !measureLight;
+    measureLight = !measureLight;
 
     if (measureLight) {
-        var light_raw = env.light.raw_value();
-        var light_value = env.light.value();
-        if (light_raw != thingState.light.raw || light_value != thingState.light.value) {
-            console.log('[EVENT]: Reading Light:', 'raw(' + light_raw + ') ~= ' + light_value + ' lux');
-            thingState.light.raw = light_raw;
-            thingState.light.value = light_value;
-            refreshShadow();
+        var temp = {
+            light: {
+                raw: env.light.raw_value(),
+                value: env.light.value()
+            }
+        };
+        console.log('[EVENT] Reading Light:', 'raw(' + temp.light.raw + ') ~= ' + temp.light.value + ' lux');
+        if (temp.light.raw != thingState.light.raw || temp.light.value != thingState.light.value) {
+            thingState.light.raw = temp.light.raw;
+            thingState.light.value = temp.light.value;
+            refreshShadow(temp);
         }
     } else {
-        var uv_volts = env.uv.volts();
-        var uv_intensity = env.uv.intensity();
-        if (uv_volts != thingState.uv.volts || uv_intensity != thingState.uv.intensity) {
-            console.log('[EVENT]: Reading UV:', uv_volts, 'V,', uv_intensity, 'mW/m^2');
-            thingState.uv.volts = uv_volts;
-            thingState.uv.intensity = uv_intensity;
-            refreshShadow();
+        var temp = {
+            uv: {
+                volts: env.uv.volts(),
+                intensity: env.uv.intensity()
+            }
+        };
+        console.log('[EVENT] Reading UV:', temp.uv.volts, 'V,', temp.uv.intensity, 'mW/m^2');
+        if (temp.uv.volts != thingState.uv.volts || temp.uv.intensity != thingState.uv.intensity) {
+            thingState.uv.volts = temp.uv.volts;
+            thingState.uv.intensity = temp.uv.intensity;
+            refreshShadow(temp);
         }
     }
 
-}, 15000);
+}, LIGHT_FREQ * 1000);
 
 
 
@@ -111,8 +146,9 @@ thingShadow.on('connect', function() {
         persistentSubscribe: true
     }, function() {
         console.log('[IOT EVENT] thingShadow.register: registered');
-        // TODO:
-        // read the shadow, compare and update if needed
+        // thingShadow.subscribe('$aws/things/' + config.iotThingName + '/shadow/get/accepted');
+        // console.log('clientToken:', thingShadow.get(config.iotThingName));
+        refreshShadow(thingState);
     });
 });
 
@@ -132,9 +168,27 @@ thingShadow.on('error', function(err) {
     throw new Error('[ERROR] Lets crash the node code because of this error.');
 });
 
-thingShadow.on('status', function(thingName, stat, clientToken, stateObject) {
+thingShadow.on('status', function(thingName, status, clientToken, stateObject) {
     console.log('[IOT EVENT] thingShadow.on(status): thingName:', thingName);
-    console.log('[IOT EVENT] thingShadow.on(status): stat:', stat);
+    console.log('[IOT EVENT] thingShadow.on(status): status:', status);
     console.log('[IOT EVENT] thingShadow.on(status): clientToken:', clientToken);
     console.log('[IOT EVENT] thingShadow.on(status): stateObject:', stateObject);
+});
+
+thingShadow.on('message', function(topic, payload) {
+    console.log('[IOT EVENT] thingShadow.on(message): on', topic, 'with', payload);
+});
+thingShadow.on('delta', function(thingName, stateObject) {
+    console.log('[IOT EVENT] thingShadow.on(delta): on', thingName, 'with', stateObject);
+    if (stateObject.state.text !== undefined) {
+        thingState.text = stateObject.state.text;
+        refreshLCD(thingState.text);
+        refreshShadow({
+            text: thingState.text
+        });
+    }
+
+});
+thingShadow.on('timeout', function(thingName, clientToken) {
+    console.log('[IOT EVENT] thingShadow.on(timeout): on', thingName, 'with token', clientToken);
 });
